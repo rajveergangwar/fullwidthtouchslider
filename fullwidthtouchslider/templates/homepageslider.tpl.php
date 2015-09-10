09b84ca554ce13531e5fbf74069ad497ffa8f545
Rajveer Singh <rajveers@localhost.localdomain>
2015-09-10 13:15:33 +0530

    unused files has been deleted

--- fullwidthtouchslider/templates/homepageslider.tpl.php
+++ fullwidthtouchslider/templates/homepageslider.tpl.php
@@ -1,8 +1,3 @@
-<?php
-drupal_add_js(drupal_get_path('module', 'fullwidthtouchslider') . '/js/jssor.js');
-drupal_add_js(drupal_get_path('module', 'fullwidthtouchslider') . '/js/jssor.slider.js');
-drupal_add_js(drupal_get_path('module', 'fullwidthtouchslider') . '/js/custom.js');
-?>
 <div id="slider1_container" style="position: relative; margin: 0 auto;
      top: 0px; left: 0px; width: 1300px; height: 500px; overflow: hidden;">
     <!-- Loading Screen -->
@@ -21,7 +16,7 @@ drupal_add_js(drupal_get_path('module', 'fullwidthtouchslider') . '/js/custom.js
             <div>
                 <img u="image" src="<?php print file_create_url(file_load($value->slide_fid)->uri) ?>" />
                 <?php if (isset($value->image_fid) && $value->image_fid != "") { ?>
-                <div class="slide_text" u="caption" t="NO" t3="RTT|2" r3="137.5%" du3="2000" d3="2500" style="position: absolute; width: 445px; height: 300px; top: 100px; left: 600px;">
+                    <div class="slide_text" u="caption" t="NO" t3="RTT|2" r3="137.5%" du3="2000" d3="2500" style="position: absolute; width: 555px; height: 300px; top: 100px; left: 600px;">
                         <?php print $value->image_fid; ?>
                     </div>
                 <?php } ?>
@@ -45,40 +40,3 @@ drupal_add_js(drupal_get_path('module', 'fullwidthtouchslider') . '/js/custom.js
     <span u="arrowright" class="jssora21r" style="top: 123px; right: 8px;">
     </span>
 </div>
-<style>
-    .jssorb21 {
-        position: absolute;
-    }
-    .jssorb21 div, .jssorb21 div:hover, .jssorb21 .av {
-        position: absolute;
-        width: 19px;
-        height: 19px;
-        text-align: center;
-        line-height: 19px;
-        color: white;
-        font-size: 12px;
-        background: url(<?php print drupal_get_path('module', 'fullwidthtouchslider');?>/img/b21.png) no-repeat;
-        overflow: hidden;
-        cursor: pointer;
-    }
-    .jssorb21 div { background-position: -5px -5px; }
-    .jssorb21 div:hover, .jssorb21 .av:hover { background-position: -35px -5px; }
-    .jssorb21 .av { background-position: -65px -5px; }
-    .jssorb21 .dn, .jssorb21 .dn:hover { background-position: -95px -5px; }
-    .jssora21l, .jssora21r {
-        display: block;
-        position: absolute;
-        width: 55px;
-        height: 55px;
-        cursor: pointer;
-        background: url(<?php print drupal_get_path('module', 'fullwidthtouchslider');?>/img/a21.png) center center no-repeat;
-        overflow: hidden;
-    }
-    .jssora21l { background-position: -3px -33px; }
-    .jssora21r { background-position: -63px -33px; }
-    .jssora21l:hover { background-position: -123px -33px; }
-    .jssora21r:hover { background-position: -183px -33px; }
-    .jssora21l.jssora21ldn { background-position: -243px -33px; }
-    .jssora21r.jssora21rdn { background-position: -303px -33px; }
-    .slide_text{color:#efdcdc}
-</style>
